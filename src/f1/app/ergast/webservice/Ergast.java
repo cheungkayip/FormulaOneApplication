39ed15d15c9dbcb47c7176b74a136039819264cb
Ka Yip Cheung <Kayip@detesters.nl>
2015-12-01 14:30:39 +0100

    Removed Target generated files

--- src/f1/app/ergast/webservice/Ergast.java
+++ src/f1/app/ergast/webservice/Ergast.java
@@ -2,14 +2,18 @@ package f1.app.ergast.webservice;
 
 import org.json.simple.JSONObject;
 
+import javax.ws.rs.Path;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
  * Created by kayipcheung on 01-12-15.
  */
-//@Path("/json/product")
+@Path("/json/product")
 public class Ergast {
-//    try{
-//        URL url = new URL()
-//    }
+
+
+
+
 }
