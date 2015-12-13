f7eb139c32a90251d6e7fa58168976229a3ae4f9
Edwin Derks <ederks85@gmail.com>
2015-12-13 15:56:09 +0100

    #32 rest-api drivers endpoint now returns real list of driver data in JSON format

--- formula-one-application-rest-api/src/main/java/f1/app/rest/endpoint/DriverEndpoint.java
+++ formula-one-application-rest-api/src/main/java/f1/app/rest/endpoint/DriverEndpoint.java
@@ -1,9 +1,14 @@
 package f1.app.rest.endpoint;
 
+import java.io.InputStream;
+import java.net.URL;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.apache.commons.io.IOUtils;
 
 /**
  * @author edwin
@@ -11,10 +16,19 @@ import javax.ws.rs.core.MediaType;
 @Path("/drivers")
 public class DriverEndpoint {
 
+	/**
+	 * @return The data available on all F1 drivers.
+	 */
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String get() {
-		System.out.println("GET");
-		return ("{test:test}");
+	public String get() throws Exception {
+		final String driversDataURL = "http://ergast.com/api/f1/2015/drivers.json";
+
+		final InputStream is = new URL(driversDataURL).openStream();
+		try {
+			return IOUtils.toString(is);
+		} finally {
+			IOUtils.closeQuietly(is);
+		}
 	}
 }
